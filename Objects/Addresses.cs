576c3afa523089a0fa52ad95ffdf088e4d25fb23
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-02-10 08:45:42 -0800

    finish methods in addresses.cs file

--- Objects/Addresses.cs
+++ Objects/Addresses.cs
@@ -21,7 +21,71 @@ namespace Addresses.Objects
       _instances.Add(this);
       _id = instances.Count;
     }
-    
+//Getters and Setters
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public string GetStreet()
+    {
+      return _street;
+    }
+
+    public void SetStreet(string newStreet)
+    {
+      _street = newStreet;
+    }
+
+    public string GetCity()
+    {
+      return _city;
+    }
+
+    public void SetCity(string newCity)
+    {
+      _city = newCity;
+    }
+
+    public string GetState()
+    {
+      return _state;
+    }
+
+    public void SetState(string newState)
+    {
+      _state = newState;
+    }
+
+    public int GetZip()
+    {
+      return _zip;
+    }
+
+    public void SetZip(int newZip)
+    {
+      _zip = newZip;
+    }
+//Other Methods
+    public static List<Address> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Address Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
 
   }
 }
