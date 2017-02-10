0968d351140d9ac397dcc76929403015f3e860cc
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-02-10 09:07:12 -0800

    add a few more get and post codes to HomeModule

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,7 +12,18 @@ namespace Addresses
         List<Address> allAddresses = Address.GetAll();
         return View["index.cshtml", allAddresses];
       };
-
+      Get["/addresses/new"] = _ => {
+        return View["new_address.cshtml"];
+      };
+      Get["/addresses/{id}"] = parameters => {
+        Address address = Address.Find(parameters.id);
+        return View["address.cshtml", address];
+      };
+      Post["/"] = _ => {
+        Address newAddress = new Address(Request.Form["new-name"]);
+        List<Address> allAddresses = Address.GetAll();
+        return View["index.cshtml", allAddresses];
+      };
 
 
     }
